b245778f805b6b2a68b3502c6b083761199fea7a
Michael Amos <michael.amos2@nike.com>
2015-08-24 10:23:47 -0700

    adding xo, fixing some lints, added a package.json

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -5,26 +5,27 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var del = require('del');
 
-gulp.task('clean', function(done){
-	del(['dist']);
+gulp.task('clean', function (done) {
+	del(['dist'], done);
 });
 
-gulp.task('webpack:build', function(done){
+gulp.task('webpack:build', function (done) {
 	var conf = Object.create(webpackConfig);
 	conf.plugins = conf.plugins.concat(
 		new webpack.optimize.DedupePlugin(),
 		new webpack.optimize.UglifyJsPlugin()
 	);
 
-	webpack(conf, function(err, stats){
-		if(err) throw new gutil.PluginError("webpack:build", err);
-			gutil.log("[webpack:build]", stats.toString({
-				colors: true
-			}));
-			done();
+	webpack(conf, function (err, stats) {
+		if (err) {
+			throw new gutil.PluginError('webpack:build', err);
+		}
+		gutil.log('[webpack:build]', stats.toString({
+			colors: true
+		}));
+		done();
 	});
 });
-
 
 // Webpack Development Server
 var devConfig = Object.create(webpackConfig);
@@ -32,21 +33,23 @@ devConfig.devtool = 'sourcemap';
 devConfig.debug = true;
 var compiler = webpack(devConfig);
 
-gulp.task('webpack:build-dev', function(done){
-	compiler.run(function(err, stats){
-		if(err) throw new gutil.PluginError("webpack:build-dev", err);
-		gutil.log("[webpack:build-dev]", stats.toString({
+gulp.task('webpack:build-dev', function (done) {
+	compiler.run(function (err, stats) {
+		if (err) {
+			throw new gutil.PluginError('webpack:build-dev', err);
+		}
+		gutil.log('[webpack:build-dev]', stats.toString({
 			colors: true
 		}));
 		done();
 	});
 });
 
-gulp.task('webpack-dev-server', function(){
+gulp.task('webpack-dev-server', function () {
 	// modify some webpack config options
 
 	var myConfig = Object.create(webpackConfig);
-	myConfig.devtool = "eval";
+	myConfig.devtool = 'eval';
 	myConfig.debug = true;
 	myConfig.plugins = myConfig.plugins.concat(new webpack.HotModuleReplacementPlugin());
 
@@ -56,9 +59,11 @@ gulp.task('webpack-dev-server', function(){
 		stats: {
 			colors: true
 		}
-	}).listen(8080, "localhost", function(err) {
-		if(err) throw new gutil.PluginError("webpack-dev-server", err);
-		gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+	}).listen(8080, 'localhost', function (err) {
+		if (err) {
+			throw new gutil.PluginError('webpack-dev-server', err);
+		}
+		gutil.log('[webpack-dev-server]', 'http://localhost:8080/webpack-dev-server/index.html');
 	});
 });
 
